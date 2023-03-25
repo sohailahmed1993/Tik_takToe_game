9757fdf8ad156f49358a0093a55c1833ca297ded
sohailahmed1993 <sohail1993ahmad@gmail.com>
2023-03-25 14:51:19 +0530

    my first project

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 let gameContainer = document.getElementById("game-container");
 let singContainer = document.getElementById("sign-container");
+let choseContainer = document.getElementById("chose-container");
+let radio1EL = document.getElementById("radio1");
+let radio2EL = document.getElementById("radio2");
+let signBtn = document.getElementById("sign-btn");
+let friendBtn = document.getElementById("friend-btn");
+
+
 let buttonEl = document.getElementById("button-btn").addEventListener('click', function(){
     singContainer.style.display = "block";
     gameContainer.style.display = "none"
 });
 
+radio1EL.addEventListener("click", function(){
+    document.getElementById("player3").style.opacity = "1";
+    document.getElementById("player4").style.opacity = "0.5";
+
+})
+radio2EL.addEventListener("click", function(){
+    document.getElementById("player3").style.opacity = "0.5"
+    document.getElementById("player4").style.opacity = "1";
+})
+
+signBtn.addEventListener("click",function(){
+    singContainer.style.display = "none";
+    choseContainer.style.display = "block"
+})
+
+friendBtn.addEventListener("click" , function(){
+    document.getElementById("localPlayer-container").style.display = "block";
+    gameContainer.style.display = "none"  
+})
